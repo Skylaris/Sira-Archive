e6cca0309848b94cda9416beac28e59aec203268
Inv Technologies <37981347+Inv-Technologies@users.noreply.github.com>
2018-04-07 11:40:39 -0400

    Update sira.py

--- sira.py
+++ sira.py
@@ -2,33 +2,26 @@ import discord
 from discord.ext import commands
 import os
 from discord.ext.commands import Bot
-prefix = [">>", ">", "->"]
+prefix = ["wiki."]
 bot = commands.Bot(command_prefix=prefix)
 bot.remove_command("help")
 
 @bot.event
 async def on_ready():
-    await bot.change_presence(game=discord.Game(name="Use >>help for help. "))
+    await bot.change_presence(game=discord.Game(name="Use wiki. for commands "))
     print("Bot online")
 
 
-@bot.command()
-async def hi():
-    await bot.say("Idk")
-    print("Done")
- 
-#invite
+
 #https://discordapp.com/oauth2/authorize?client_id=430003755405279242&scope=bot
-global banned_words
-banned_words = "banned_words.txt"
-try:
-    f = open(banned_words)
-    greetings = f.read().splitlines()
-    f.close
-    print(f)
-except IOError:
-    print("Couldn't find file " + banned_words)
     
+@bot.command(pass_context=True)
+async def battleon(ctx):
+    
+    embed = discord.Embed(title="Battleon", color=0x00a0ea)
+    embed.set_thumbnail(url = "https://thumb.ibb.co/euN8un/AQ3_D_Logo_T_shirt.png")
+await bot.say(embed=embed)
+
 
 token = os.environ.get("sira")
 bot.run(f'{token}')
